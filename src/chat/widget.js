82e8e152f55271f053697ccbb0683db24b986f01
rjdw <rjdw@rjdws-MacBook-Pro.local>
2025-04-28 05:23:18 -0700

    prod chat, no python server in prod yet

--- src/chat/widget.js
+++ src/chat/widget.js
@@ -100,37 +100,46 @@ class GitterCommFloating extends HTMLElement {
     this.appendMessage("bot", "⏳ …");
     const placeholder = this.$chat.lastChild;
 
+    // try {
+    //   const request = await axios.get("http://localhost:5000/api/promptedMsg", {
+    //     params: {
+    //       user_input: msg,
+    //       msg_counter: this.msg_counter,
+    //       discrete: this.toggleOn,
+    //     },
+    //   });
+
+    //   const prompted_msg = request.data.prompt;
+    //   const promptType = request.data.promptType;
+
+    //   if (promptType == "clean") {
+    //     this.msg_counter += 1;
+    //   } else {
+    //     this.msg_counter = 0;
+    //   }
+    //   console.log(this.msg_counter, promptType);
+    //   const reply = await callGemini(prompted_msg);
+    //   placeholder.remove();
+
+    //   // let replyPrefix = "";
+    //   // if (this.toggleOn == false && promptType == "injected") {
+    //   //   replyPrefix = `<div><p>Sponsored</p><br></div>`;
+    //   // }
+
+    //   // const finalReply = replyPrefix + `<div>${reply}</div>`;
+
+    //   const finalReply = `<div>${reply}</div>`;
+
+    //   this.appendMessage("bot", finalReply);
+    // } catch (err) {
+    //   placeholder.remove();
+    //   this.appendMessage("bot", "Error: " + err.message);
+    // }
+
     try {
-      const request = await axios.get("http://localhost:5000/api/promptedMsg", {
-        params: {
-          user_input: msg,
-          msg_counter: this.msg_counter,
-          discrete: this.toggleOn,
-        },
-      });
-
-      const prompted_msg = request.data.prompt;
-      const promptType = request.data.promptType;
-
-      if (promptType == "clean") {
-        this.msg_counter += 1;
-      } else {
-        this.msg_counter = 0;
-      }
-      console.log(this.msg_counter, promptType);
-      const reply = await callGemini(prompted_msg);
+      const reply = await callGemini(msg);
       placeholder.remove();
-
-      // let replyPrefix = "";
-      // if (this.toggleOn == false && promptType == "injected") {
-      //   replyPrefix = `<div><p>Sponsored</p><br></div>`;
-      // }
-
-      // const finalReply = replyPrefix + `<div>${reply}</div>`;
-
-      const finalReply = `<div>${reply}</div>`;
-
-      this.appendMessage("bot", finalReply);
+      this.appendMessage("bot", reply);
     } catch (err) {
       placeholder.remove();
       this.appendMessage("bot", "Error: " + err.message);
